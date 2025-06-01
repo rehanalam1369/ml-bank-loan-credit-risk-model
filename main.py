d1d2904969a86989001f438099ea29e9a2398499
rehanalam1369 <rehanalam1369@gmail.com>
2025-06-01 15:49:35 +0530

    Update main.py
    Naming for model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from prediction_helper import predict  # Ensure this is correctly linked to your prediction_helper.py
 
 # Set the page configuration and title
-st.set_page_config(page_title="Lauki Finance: Credit Risk Modelling", page_icon="📊")
+st.set_page_config(page_title="Bajaj Finance: Credit Risk Modelling", page_icon="📊")
 st.title("Bajaj Finance: Credit Risk Modelling")
 
 # Create rows of three columns each
